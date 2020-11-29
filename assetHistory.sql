fa99073d801da0fef9f44193a0e2bbd6e68a87c6
vtortonesi <75135187+vtortonesi@users.noreply.github.com>
2020-11-29 00:00:03 -0500

    Update assetHistory.sql

--- assetHistory.sql
+++ assetHistory.sql
@@ -1,13 +1,11 @@
-SELECT tc.tcname 'Device Name', 
-	   ai.Connector 'Connector Type', 
-	   ai.Vendor 'Asset Vendor', 
-	   ai.Device_Name 'Asset Deivce Name', 
-	   ah.event_Timestamp 'Event Timestamp', 
-	   ah.Event_Type ' Event Type'
+SELECT 
+tc.tcname 'Device Name', 
+ai.Connector 'Connector Type', 
+ai.Vendor 'Asset Vendor', 
+ai.Device_Name 'Asset Deivce Name', 
+ah.event_Timestamp 'Event Timestamp', 
+ah.Event_Type ' Event Type'
 
 FROM igelums.thinclient tc
-JOIN igelums.asset_history ah
-ON tc.unit_id = ah.unit_id
+JOIN igelums.asset_history ah ON tc.unit_id = ah.unit_id
 JOIN igelums.asset_info ai ON ah.asset_id = ai.asset_id
-
-WHERE ah.EVENT_TIMESTAMP BETWEEN (@startTime) AND (@endtime)
