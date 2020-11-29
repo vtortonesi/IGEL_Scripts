45c746500ef7f9210359d873e5f240ec1c71847b
vtortonesi <75135187+vtortonesi@users.noreply.github.com>
2020-11-29 00:14:15 -0500

    Update profileAssignedToDeviceByGroup.sql

--- profileAssignedToDeviceByGroup.sql
+++ profileAssignedToDeviceByGroup.sql
@@ -1,16 +1,13 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
 SELECT
- 	tc.tcname AS 'Device',
-	p.[name] 'Profile Name'
-
+tc.tcname AS 'Device',
+p.[name] 'Profile Name'
 
 FROM igelums.PROFILES p JOIN igelums.DIRECTORYHASPROFILE dp ON p.profileid = dp.profileid 
-JOIN igelums.DIRECTORIES d ON dp.dirid = d.dirid
 
+JOIN igelums.DIRECTORIES d ON dp.dirid = d.dirid
 JOIN igelums.THINCLIENTISINDIRECTORY tcd ON d.DIRID = tcd.DIRID
 JOIN igelums.THINCLIENT tc ON tcd.TCID = tc.TCID
 
-WHERE d.[NAME] = @directoryname
 
 
 
