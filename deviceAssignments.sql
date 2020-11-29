329e222eab00838a19a9ed3e2d7d9bb3d5129416
vtortonesi <75135187+vtortonesi@users.noreply.github.com>
2020-11-29 00:10:48 -0500

    Update deviceAssignments.sql

--- deviceAssignments.sql
+++ deviceAssignments.sql
@@ -1,11 +1,9 @@
-SELECT tc.tcname AS 'Device',
-	tc.LASTBOOTTIME AS 'Last Restart',
-	tc.PROTOCOL AS 'Connection Type',
-                d.[name] AS 'Device Group'
+SELECT 
+tc.tcname AS 'Device',
+tc.LASTBOOTTIME AS 'Last Restart',
+tc.PROTOCOL AS 'Connection Type',
+d.[name] AS 'Device Group'
 		 
-FROM 	igelums.thinclient tc
-	JOIN igelums.THINCLIENTISINDIRECTORY tid ON tc.TCID = tid.TCID
-	JOIN igelums.DIRECTORIES d ON tid.DIRID = d.DIRID
-
-WHERE d.Name = @devicegroup 
-AND tc.PROTOCOL = @protocol
+FROM  igelums.thinclient tc
+JOIN igelums.THINCLIENTISINDIRECTORY tid ON tc.TCID = tid.TCID
+JOIN igelums.DIRECTORIES d ON tid.DIRID = d.DIRID
