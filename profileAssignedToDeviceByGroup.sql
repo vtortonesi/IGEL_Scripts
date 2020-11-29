7cc3ac8a7184463f9466c13e394a95645c49200e
vtortonesi <75135187+vtortonesi@users.noreply.github.com>
2020-11-29 00:13:16 -0500

    Add files via upload

---
+++ profileAssignedToDeviceByGroup.sql
@@ -0,0 +1,16 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT
+ 	tc.tcname AS 'Device',
+	p.[name] 'Profile Name'
+
+
+FROM igelums.PROFILES p JOIN igelums.DIRECTORYHASPROFILE dp ON p.profileid = dp.profileid 
+JOIN igelums.DIRECTORIES d ON dp.dirid = d.dirid
+
+JOIN igelums.THINCLIENTISINDIRECTORY tcd ON d.DIRID = tcd.DIRID
+JOIN igelums.THINCLIENT tc ON tcd.TCID = tc.TCID
+
+WHERE d.[NAME] = @directoryname
+
+
+
---
+++ profilesdirectlyassigned.sql
@@ -0,0 +1,9 @@
+SELECT tc.tcname 'Device Name',
+	   p.[Name] AS 'Profile',
+	   COALESCE (ps.Classname, 'No Profile Setting') AS 'Profile Setting',
+	   COALESCE (ps.PValue, 'No Profile Value') AS 'Profile Value'
+FROM igelums.THINCLIENTHASPROFILE tcp JOIN igelums.THINCLIENT tc
+ON tcp.TCID = tc.TCID
+JOIN igelums.PROFILES p ON tcp.PROFILEID = p.PROFILEID
+
+LEFT JOIN igelums.PROFILESETTINGS ps ON p.PROFILEID = ps.PROFILEID
